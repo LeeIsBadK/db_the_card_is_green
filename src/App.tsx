da72245b3d49fe2bd2d76193d513bcb381ce5603
dadada10x <tataleesbk@gmail.com>
2024-03-23 00:19:11 +0700

    set 23 Mar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
 import Navbbar from './assets/component/navbar'
-
 import supabase from './server/App'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<any>([])
+  useEffect(() => {
+    console.log(supabase)
+    getData()
+  },[])
+
+  async function getData() {
+    const { data, error } = await supabase.from('Employee').select();
+    if (error) {
+      console.log('Error fetching data:', error.message)
+      return
+    }
+    console.log(data)
+    setData(data);
+  }
 
   return (
     <>
-      {console.log(supabase)}
       {Navbbar()}
-      <div>
-        
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-          dadadada
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
