a7d9d20c6565b42c33cea41f331abed62e621eb6
Kittiphat Leesombatwathana <79010876+LeeIsBadK@users.noreply.github.com>
2024-03-25 00:48:43 +0700

    Inseart data completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import { useEffect, useState } from 'react'
-import Navbbar from './assets/component/navbar'
-import supabase from './server/App'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+
+import Home from "./pages/home";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
 
 function App() {
-  const [data, setData] = useState<any>([])
-  useEffect(() => {
-    console.log(supabase)
-    getData()
-  },[])
-
-  async function getData() {
-    const { data, error } = await supabase.from('Employee').select();
-    if (error) {
-      console.log('Error fetching data:', error.message)
-      return
-    }
-    console.log(data)
-    setData(data);
-  }
-
   return (
-    <>
-      {Navbbar()}
-    </>
-  )
+    <BrowserRouter>
+      <nav className="Box w-full h-20 bg-emerald-600">
+        <ul className="flex justify-center items-center h-full">
+          <li className="mx-4 ">
+            <Link to="/" className="text-white">
+              Home
+            </Link>
+          </li>
+          <li className="mx-4">
+            <Link to="/create" className="text-white">
+              Create
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
---
+++ src/pages/Update.tsx
@@ -0,0 +1,8 @@
+function Update() {
+    return(
+        <div>
+            <h1>Update</h1>
+        </div>
+    )
+}
+export default Update;
