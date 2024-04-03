40f08bdc1795856543afc492cca0314b4edde81d
dadada10x <tataleesbk@gmail.com>
2024-04-03 23:37:18 +0700

    setting Auth on supabase 4

--- src/server/App.ts
+++ src/server/App.ts
@@ -3,5 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_PROJECT_URL || ""
 const supabaseKey = import.meta.env.VITE_SVDX|| ""
+console.log(supabaseUrl)
+console.log(supabaseKey)
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
