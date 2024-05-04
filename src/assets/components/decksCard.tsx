d3fc8af3c7a56275334db67ff3048201cfdbea82
Kittiphat Leesombatwathana <79010876+LeeIsBadK@users.noreply.github.com>
2024-05-04 17:01:24 +0700

    Checkpoint 4 May 2024

--- src/assets/components/decksCard.tsx
+++ src/assets/components/decksCard.tsx
@@ -6,7 +6,7 @@ const gachaCard = (decks: any) => {
             <div
             className="cursor-pointer transition-all duration-500 hover:translate-y-2 hover:bg-stone-600 h-40 bg-stone-400 rounded-lg shadow-xl flex flex-row items-center justify-evenly gap-4 px-4" key={decks.id}
         >
-            <img src='.\src\assets\components\YGO-backcard.png' alt="deck" className="w-20" />
+            <img src='.\src\assets\YGO-backcard.png' alt="deck" className="w-20" />
   
             <div className="w-36">
                 <span className="font-bold text-teal-950 text-xl">{decks.name}</span>
