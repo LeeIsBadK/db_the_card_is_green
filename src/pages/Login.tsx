33d9c391b27b0968624ff10446ba33bf2fbf4436
Kittiphat Leesombatwathana <79010876+LeeIsBadK@users.noreply.github.com>
2024-05-05 02:46:49 +0700

    Checkpoint 5 May 2024

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -39,7 +39,7 @@ const Login = () => {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6">
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
                             <div className="mt-2">
                                 <input id="email" name="username" required className="block w-full rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={username} onChange={(e) => setUsername(e.target.value)} />
                             </div>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -36,9 +36,9 @@ const SignUp = () => {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6">
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
                             <div className="mt-2">
-                                <input id="email" name="username" required className="block w-full rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={username} onChange={(e) => setUsername(e.target.value)} />
+                                <input id="username" name="username" required className="block w-full rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={username} onChange={(e) => setUsername(e.target.value)} />
                             </div>
                         </div>
 
