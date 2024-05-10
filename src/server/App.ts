a79671396a5e28b0b9f9ed5675a1d84ad04de53b
Kittiphat Leesombatwathana <79010876+LeeIsBadK@users.noreply.github.com>
2024-05-10 10:44:20 +0700

    Update App.ts

--- src/server/App.ts
+++ src/server/App.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const supabaseUrl = import.meta.env.VITE_PROJECT_URL || ""
-const supabaseKey = import.meta.env.VITE_SVDX|| ""
+const supabaseKey = import.meta.env.VITE_ANON_KEY|| ""
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
